30a78affbb91819ee0d794f577a97da8121f1285
Aaron Law <aaron.law@gmail.com>
2019-06-06 09:01:16 +0800

    Define the basic structure of the script.

---
+++ create_bulk_user_account.py
@@ -0,0 +1,20 @@
+filename = 'useraccount.csv'
+num_user = 1000
+column_list = []
+
+def create_user_info(num_account):
+    '''A sample format of user information is:
+    First-name,Last-name, Displayname, EmailAddress
+    '''
+    # Made the 1st line as column head.
+    # Made the following lines.
+    pass
+
+def write_file' '   (lists,filename):
+    '''Write lists as a csv file.
+    '''
+    pass
+
+if __name__ == "__main__":
+    create_user_info(num_user)
+    write_file()
